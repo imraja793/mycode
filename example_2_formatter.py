0a26e68e9b34e69d4043225e493a9f537cbcbc45
rajadhanuk <raja@dataplusweb.com>
2021-01-28 18:28:52 +0530

    exercises

---
+++ example_2_formatter.py
@@ -0,0 +1,26 @@
+formattter = "{} {} {} {}"
+print(formattter.format(1,2,3,4), "formatting integers")
+print(formattter.format('a','b','c','d'), "formatting strings remember to put colon for string")
+print(formattter.format([1,2,3,4], (1,2,3,4), ['a','s','f','f','g'], {"asd":"dkmf", "fkfnfk":'fjbfbj'}))
+print(formattter.format(1,2,3,34,45,6))
+
+
+print("I'm \n ")
+
+print("ddnd jdn j","fkf 'ff'",  sep="d......")
+countdown = "ekiehihhehi"
+print(countdown, end='...')
+
+
+x = 100
+y = 50
+
+x = x^y
+print(f"{x} xxx")
+y = x^y
+print(f'{y} yyyy    ')
+x = x^y
+print(f'{x} X {y} Y')
+
+
+input("djnd") = x
